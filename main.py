fe6ca5fbc83432e090892259f8684a3931961b73
Ekaterina050412 <katyashmyrkova4002005@gmail.com>
2024-10-04 00:49:22 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,13 +1,5 @@
-#1st program
-print(9 ** 0.5 * 5)
-#2hd program
-print(99.9 >= 99.8 and 1000 != 1000.1)
-#3rd program
-print(2 * 2 + 2)
-print(2 * (2+2))
-print((2 * 2 + 2) == (2 * (2 + 2)))
-#4th program
-print(float('123.456'))
-print(123.456 * 10)
-print(int(1234.56))
-print(1234 % 10)
+print('Hi, PyCharm')
+x = 43
+y = 32
+print(x * y)
+print("End line")
